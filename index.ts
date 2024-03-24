a321eea9dd782a7d0781a37fe6211200a858e905
Aqeeq Ahmed <aqeeq.ahmed@junaidjamshed.com>
2024-03-24 17:20:30 +0500

    tweak the text and added console at the end

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
   {
     message: "Select one of the operator to perform operation",
     type: "list",
@@ -21,3 +21,4 @@ if (answer.operator === "Add") {
 } else if (answer.operator === "Div") {
   console.log(answer.firstNumber / answer.secondNumber);
 }
+console.log("The End");
